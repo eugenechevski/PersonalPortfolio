778dd833d10d87749872a7102d0fcc5c1a3ce35b
Eugene Chevski <eugenechevski@proton.me>
2023-05-20 19:30:58 -0400

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import { Lato } from "next/font/google";
+
+const lato = Lato({ weight: "400", subsets: ["latin-ext"] });
+
+export const metadata = {
+  title: "Eugene Chevski - Software Engineer",
+  description:
+    "A personal portfolio website to showcase my experience and create the contact point for potential employers or business partners.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={lato.className}>{children}</body>
+    </html>
+  );
+}
