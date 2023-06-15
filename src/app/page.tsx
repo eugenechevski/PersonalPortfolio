820378defd1bc8c736b52ca55a673814434187df
Eugene Chevski <eugenechevski@proton.me>
2023-06-14 21:33:50 -0400

    Removes styled components from the project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
-"use client";
-
 import Image from "next/image";
 import Link from "next/link";
 
-import tw from "tailwind-styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faGithub,
@@ -43,243 +40,10 @@ const projects = [
 ];
 
 export default function Page() {
-  /**
-   * Styled components
-   */
-
-  const MainContainer = tw.main`
-    w-100vw
-    max-h-max
-    text-white
-    opacity-[50%]
-    scroll-smooth
-    transition-all
-    duration-500
-    ease-in-out
-  `;
-
-  const Intro = tw.header`
-    h-[100vh]
-    flex
-    flex-col
-    items-center
-    justify-center
-    gap-12
-  `;
-
-  const IntroHeading = tw.h1`
-    text-2xl
-    text-center
-    sm:text-6xl
-    font-bold
-  `;
-
-  const IntroImageContainer = tw.figure`
-    rounded-full
-    bg-purple-500
-    w-[250px]
-    h-[250px]
-    sm:w-[500px]
-    sm:h-[500px]
-    overflow-hidden
-    shadow-2xl
-    drop-shadow-2xl
-  `;
-
-  const About = tw.section`
-    flex
-    flex-col
-    sm:flex-row
-    items-center
-    justify-center
-    gap-12
-    p-12
-  `;
-
-  const AboutImageContainer = tw.figure`
-    shadow-2xl
-    drop-shadow-2xl
-    w-[250px]
-    sm:w-[500px]
-    sm:mb-6
-  `;
-
-  const AboutContent = tw.article`
-    flex
-    flex-col
-    items-center
-    justify-center
-    gap-6
-    sm:gap-12
-    lg:w-1/2
-  `;
-
-  const SectionHeading = tw.h1`
-    text-3xl
-    text-center
-    sm:text-6xl
-    font-bold
-  `;
-  const AboutText = tw.p`
-    text-start
-    text-sm
-    sm:text-xl
-    font-light
-    lg:w-3/4
-    drop-shadow-2xl
-  `;
-
-  const FeaturedProjects = tw.section`
-    min-h-[100vh]
-    flex
-    flex-col
-    gap-12
-    items-center
-    justify-evenly
-  `;
-
-  const ProjectsContainer = tw.div`
-    flex
-    flex-col
-    sm:flex-row
-  `;
-
-  const Project = tw.article`
-    flex
-    flex-col
-    items-center
-    justify-center
-    gap-6
-  `;
-
-  const ProjectImageContainer = tw.figure`
-    flex
-    items-center
-    rounded-full
-    w-[150px]
-    h-[150px]
-    sm:w-[300px]
-    sm:h-[300px]
-    shadow-2xl
-    drop-shadow-2xl
-    overflow-hidden
-  `;
-
-  const ProjectTitle = tw.h1`
-    text-2xl
-    text-center
-  `;
-
-  const ProjectDescription = tw.p`
-    text-md
-    text-center
-    w-3/4
-  `;
-
-  const ExploreButton = tw.button`
-    w-64
-    p-4
-    bg-white
-    rounded-3xl
-    shadow-2xl
-    drop-shadow-2xl
-    text-[#6B21A5]
-    text-xl
-    mb-12
-  `;
-
-  const SocialMedia = tw.section`
-    flex
-    flex-col
-    items-center
-    justify-center
-    gap-12
-    min-h-[100vh]
-  `;
-
-  const SocialMediaLinks = tw.div`
-    flex  
-    items-center
-    justify-center
-    gap-12
-    h-[60vh]
-  `;
-
-  const Contact = tw.section`
-    flex
-    flex-col
-    justify-center
-    items-center
-    sm:gap-12
-  `;
-
-  const ContactMessageContainer = tw.article`
-    flex
-    flex-col
-    items-center
-    justify-start
-    gap-6
-    h-[30vh]
-    sm:h-[15vh]
-  `;
-
-  const ContactMessageParagraph = tw.p`
-    text-md
-    h-3/4
-    text-center
-    sm:text-start
-  `;
-
-  const ContactForm = tw.form`
-    flex
-    flex-col
-    gap-4
-    sm:w-1/2
-    lg:w-1/6
-  `;
-
-  const Input = tw.input`
-    bg-white
-    outline-none
-    text-black
-    p-3
-    rounded-xl
-  `;
-
-  const Label = tw.label`
-    text-md
-  `;
-
-  const TextArea = tw.textarea`
-    bg-white
-    outline-none
-    text-black
-    p-3
-    rounded-xl
-    resize-none
-  `;
-
-  const SubmitButton = tw.button`
-    w-64
-    p-2
-    bg-white
-    rounded-3xl
-    shadow-2xl
-    drop-shadow-2xl
-    text-[#6B21A5]
-    text-xl
-    mb-12
-    self-center
-  `;
-
-  /**
-   * State
-   */
-
   return (
-    <MainContainer>
-      <Intro>
-        <IntroImageContainer>
+    <main className="w-100vw max-h-max text-white opacity-50 scroll-smooth transition-all duration-500 ease-in-out">
+      <header className="h-[100vh] flex flex-col items-center justify-center gap-12">
+        <figure className="rounded-full bg-purple-500 w-[250px] h-[250px] sm:w-[500px] sm:h-[500px] overflow-hidden shadow-2xl drop-shadow-2xl">
           <Image
             src={introImg}
             alt="introduction photo"
@@ -289,16 +53,15 @@ export default function Page() {
             width={500}
             height={500}
           />
-        </IntroImageContainer>
-        <IntroHeading>
+        </figure>
+        <h1 className="text-2xl text-center sm:text-6xl font-bold">
           Hello, I am Eugene Chevski - a full-stack software engineer.
-        </IntroHeading>
-      </Intro>
-      <About>
-        <AboutImageContainer>
+        </h1>
+      </header>
+      <section className="flex flex-col sm:flex-row items-center justify-center gap-12 p-12">
+        <figure className="shadow-2xl drop-shadow-2xl w-[250px] sm:w-[500px] sm:mb-6">
           <Image
-            className="shadow-2xl
-                       drop-shadow-2xl"
+            className="shadow-2xl drop-shadow-2xl"
             src={aboutImg}
             alt="about photo"
             sizes="(max-width: 576px) 250px, 500px"
@@ -307,10 +70,12 @@ export default function Page() {
             width={500}
             height={500}
           />
-        </AboutImageContainer>
-        <AboutContent>
-          <SectionHeading>About me</SectionHeading>
-          <AboutText>
+        </figure>
+        <article className="flex flex-col items-center justify-center gap-6 sm:gap-12 lg:w-1/2">
+          <h1 className="text-3xl text-center sm:text-6xl font-bold">
+            About me
+          </h1>
+          <p className="text-start text-sm sm:text-xl font-light lg:w-3/4 drop-shadow-2xl">
             I am a passionate software engineer specializing in innovative and
             user-friendly application development. My expertise spans
             JavaScript, HTML, CSS, React, TypeScript, Tailwind, MongoDB, SQL,
@@ -320,15 +85,20 @@ export default function Page() {
             continuously learn and grow. I am confident that my skills and
             experience would make me a valuable asset to any team. Feel free to
             explore my portfolio for more information about my work.
-          </AboutText>
-        </AboutContent>
-      </About>
-      <FeaturedProjects>
-        <SectionHeading>Featured Projects</SectionHeading>
-        <ProjectsContainer>
+          </p>
+        </article>
+      </section>
+      <section className="min-h-[100vh] flex flex-col gap-12 items-center justify-evenly">
+        <h1 className="text-3xl text-center sm:text-6xl font-bold">
+          Featured Projects
+        </h1>
+        <div className="flex flex-col sm:flex-row">
           {projects.map(({ name, link, description, image }) => (
-            <Project key={uniqid()}>
-              <ProjectImageContainer>
+            <article
+              className="flex flex-col items-center justify-center gap-6"
+              key={uniqid()}
+            >
+              <figure className="flex items-center rounded-full w-[150px] h-[150px] sm:w-[300px] sm:h-[300px] shadow-2xl drop-shadow-2xl overflow-hidden">
                 <Link className="relative h-full w-full" href={link}>
                   <Image
                     className="rounded-full"
@@ -340,73 +110,108 @@ export default function Page() {
                     fill
                   />
                 </Link>
-              </ProjectImageContainer>
-              <ProjectTitle>{name}</ProjectTitle>
-              <ProjectDescription>{description}</ProjectDescription>
-            </Project>
+              </figure>
+              <h1 className="text-2xl text-center">{name}</h1>
+              <p className="text-md text-center w-3/4">{description}</p>
+            </article>
           ))}
-        </ProjectsContainer>
+        </div>
         <Link href={"https://github.com/eugenechevski"}>
-          <ExploreButton>Explore</ExploreButton>
+          <button className="w-64 p-4 bg-white rounded-3xl shadow-2xl drop-shadow-2xl text-[#6B21A5] text-xl mb-12">
+            Explore
+          </button>
         </Link>
-      </FeaturedProjects>
-      <SocialMedia>
-        <SectionHeading>Social Media</SectionHeading>
-        <SocialMediaLinks>
+      </section>
+      <section className="flex flex-col items-center justify-center gap-12 min-h-[100vh]">
+        <h1 className="text-3xl text-center sm:text-6xl font-bold">
+          Social Media
+        </h1>
+        <div className="flex items-center justify-center gap-12 h-[60vh]">
           <Link href={"https://www.instagram.com/eugenechevski/"}>
-            <FontAwesomeIcon size="3x" icon={faInstagram} />
+            <FontAwesomeIcon className="w-12" size="3x" icon={faInstagram} />
           </Link>
           <Link href={"https://twitter.com/eugenechevski7"}>
-            <FontAwesomeIcon size="3x" icon={faTwitter} />
+            <FontAwesomeIcon className="w-12" size="3x" icon={faTwitter} />
           </Link>
           <Link
             href={
               "https://www.linkedin.com/in/yauheni-khvashcheuski-181b06263/"
             }
           >
-            <FontAwesomeIcon size="3x" icon={faLinkedinIn} />
+            <FontAwesomeIcon className="w-12" size="3x" icon={faLinkedinIn} />
           </Link>
           <Link href={"https://github.com/eugenechevski"}>
-            <FontAwesomeIcon size="3x" icon={faGithub} />
+            <FontAwesomeIcon className="w-12" size="3x" icon={faGithub} />
           </Link>
-        </SocialMediaLinks>
-      </SocialMedia>
-      <Contact>
-        <ContactMessageContainer>
-          <SectionHeading>Contact me</SectionHeading>
-          <ContactMessageParagraph>
+        </div>
+      </section>
+      <section className="flex flex-col justify-center items-center sm:gap-12">
+        <article className="flex flex-col items-center justify-start gap-6 h-[30vh] sm:h-[15vh]">
+          <h1 className="text-3xl text-center sm:text-6xl font-bold">
+            Contact me
+          </h1>
+          <p className="text-md h-3/4 text-center sm:text-start">
             If you have any questions, concerns, or offers, feel free to contact
             me through this form or the social media above.
-          </ContactMessageParagraph>
-        </ContactMessageContainer>
-        <ContactForm
+          </p>
+        </article>
+        <form
+          className="flex flex-col gap-4 sm:w-1/2 lg:w-1/4"
+          method="POST"
           action={"/success"}
           name="contact"
           data-netlify="true"
           data-netlify-recaptcha="true"
         >
           <input type="hidden" name="form-name" value="contact" />
-          <Label htmlFor="first-name">First Name</Label>
-          <Input required maxLength={20} type="text" id="first-name" />
-          <Label htmlFor="last-name">Last Name</Label>
-          <Input required maxLength={20} type="text" id="last-name" />
-          <Label htmlFor="email">Email</Label>
-          <Input
+          <label className="text-md" htmlFor="first-name">
+            First Name
+          </label>
+          <input
+            className="bg-white outline-none text-black p-3 rounded-xl"
+            required
+            maxLength={20}
+            type="text"
+            id="first-name"
+          />
+          <label className="text-md" htmlFor="last-name">
+            Last Name
+          </label>
+          <input
+            className="bg-white outline-none text-black p-3 rounded-xl"
+            required
+            maxLength={20}
+            type="text"
+            id="last-name"
+          />
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="bg-white outline-none text-black p-3 rounded-xl"
             pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
             type="email"
             id="email"
           />
-          <Label htmlFor="message">Message</Label>
-          <TextArea
+          <label className="text-md" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="bg-white outline-none text-black p-3 rounded-xl resize-none"
             required
             minLength={30}
             maxLength={200}
             id="message"
             rows={10}
           />
-          <SubmitButton type="submit">Send</SubmitButton>
-        </ContactForm>
-      </Contact>
-    </MainContainer>
+          <button
+            className="w-64 p-2 bg-white rounded-3xl shadow-2xl drop-shadow-2xl text-[#6B21A5] text-xl mb-12 self-center"
+            type="submit"
+          >
+            Send
+          </button>
+        </form>
+      </section>
+    </main>
   );
 }
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,60 +1,18 @@
-
-'use client';
-
-import tw from "tailwind-styled-components";
 import Link from "next/link";
 
 export default function Page() {
-  const Container = tw.div`
-    h-screen
-    w-screen
-    flex
-    flex-col
-    items-center
-    justify-center
-    opacity-[50%]
-   `;
-
-  const MessageContainer = tw.div`
-    lg:w-1/2
-    h-1/2
-    flex
-    flex-col
-    items-center
-    justify-center
-    text-center
-    gap-12
-    p-4
-    `;
-
-  const Message = tw.h1`
-     text-3xl
-     font-bold
-     text-white
-    `;
-
-  const ReturnButton = tw.button`
-    w-64
-    p-4
-    bg-white
-    rounded-3xl
-    shadow-2xl
-    drop-shadow-2xl
-    text-[#6B21A5]
-    text-xl
-    mb-12
-  `;
-
   return (
-    <Container>
-      <MessageContainer>
-        <Message>
+    <div className="h-screen w-screen flex flex-col items-center justify-center opacity-[50%]">
+      <div className="lg:w-1/2 h-1/2 flex flex-col items-center justify-center text-center gap-12 p-4">
+        <h1 className="text-3xl font-bold text-white">
           Thank you for your message! I will be in touch soon.
-        </Message>
+        </h1>
         <Link href="/">
-          <ReturnButton>Return Home</ReturnButton>
+          <button className="w-64 p-4 bg-white rounded-3xl shadow-2xl drop-shadow-2xl text-[#6B21A5] text-xl mb-12">
+            Return Home
+          </button>
         </Link>
-      </MessageContainer>
-    </Container>
+      </div>
+    </div>
   );
 }
