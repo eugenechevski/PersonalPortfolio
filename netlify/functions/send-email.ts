cf40e301ded619e5dc602044ab3aef5ccb946a28
Eugene Chevski <eugenechevski@proton.me>
2023-06-18 22:49:44 -0400

    deletes Netlify functions directory

--- netlify/functions/send-email.ts
+++
@@ -1,7 +0,0 @@
-import type { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
-
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  // your server-side functionality
-};
-
-export { handler };
