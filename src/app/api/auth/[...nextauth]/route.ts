d3cf2cf2530ae7fef4951673d5cf6ac1a7aaff54
Eugene Chevski <eugenechevski@proton.me>
2023-08-16 22:12:20 -0400

    Adds 'pages' configuration option to next-auth.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -37,6 +37,13 @@ const handler = NextAuth({
       },
     }),
   ],
+  pages: {
+    signIn: "/auth/signin",
+    signOut: "/auth/signout",
+    error: "/auth/error", // Error code passed in query string as ?error=
+    verifyRequest: "/auth/verify-request", // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  }
 });
 
 export { handler as GET, handler as POST };
