35500cc2349cf5f0ab553dacf2eb8484552ef6a4
Eugene Chevski <eugenechevski@proton.me>
2023-08-20 16:34:03 -0400

    Creates the route handler for the users API.

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,46 @@
+// Endpoint for handling user related requests
+
+import clientPromise from "@/lib/mongodb";
+import { NextResponse } from "next/server";
+import { ObjectId } from "mongodb";
+
+export async function GET() {
+  const client = await clientPromise;
+  const db = client.db("personal_blog");
+  const users = await db.collection<AdminUser>("users").find({}).toArray();
+
+  return NextResponse.json(users);
+}
+
+export async function POST(req: Request) {
+  const user: AdminUser = await req.json();
+  const client = await clientPromise;
+  const db = client.db("personal_blog");
+  const result = await db.collection<AdminUser>("users").insertOne(user);
+
+  return NextResponse.json(result);
+}
+
+export async function PUT(req: Request) {
+  const user = (await req.json()) as AdminUser;
+  const client = await clientPromise;
+  const db = client.db("personal_blog");
+  const result = await db
+    .collection<AdminUser>("users")
+    .updateOne({ _id: new ObjectId(user.userId) }, { $set: user });
+
+  return NextResponse.json(result);
+}
+
+export async function DELETE(req: Request) {
+  // Obtain id from request query
+  const userId = await req.text();
+  const client = await clientPromise;
+  const db = client.db("personal_blog");
+
+  const result = await db.collection<AdminUser>("users").deleteOne({
+    _id: new ObjectId(userId),
+  });
+
+  return NextResponse.json(result);
+}
