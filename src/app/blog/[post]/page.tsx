de402943b9fc69486c63ee3cbaf05c44c05901c5
Eugene Chevski <eugenechevski@proton.me>
2023-08-24 21:28:02 -0400

    Adds the sharing buttons to the blog posts.

--- src/app/blog/[post]/page.tsx
+++ src/app/blog/[post]/page.tsx
@@ -1,10 +1,7 @@
 // Page for individual blog posts
 "use client";
 
-import parse, {
-  type HTMLReactParserOptions,
-  Element,
-} from "html-react-parser";
+import parse, { type HTMLReactParserOptions, Element } from "html-react-parser";
 
 import { useParams } from "next/navigation";
 
@@ -17,9 +14,25 @@ import {
 
 // Font awesome: share, comment, and like icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faShare, faComment, faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faShare,
+  faComment,
+  faHeart,
+  faClose,
+  faCopy,
+} from "@fortawesome/free-solid-svg-icons";
 
 import { useState } from "react";
+
+// next-share: sharing buttons
+import {
+  RedditShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  RedditIcon,
+  TwitterIcon,
+  WhatsappIcon,
+} from "next-share";
 
 const options: HTMLReactParserOptions = {
   replace: (domNode) => {
@@ -30,21 +43,34 @@ const options: HTMLReactParserOptions = {
       if (!src.endsWith("?raw=true")) {
         domNode.attribs.src = src + "?raw=true";
       }
+
+      // Add the priority attribute to the image
+      domNode.attribs.loading = "lazy";
     }
   },
 };
 
 export default function PostPage() {
+  const shareMessage =
+    "Found this cool blog post that's worth a read and share. Check it out! 📚😊 #BlogReads #SharingIsCaring";
+
   const postId = useParams().post as string;
+  const postUrl = "https://eugenechevski.com/blog/" + postId;
 
   const dispatch = useDispatch();
   const posts = useSelector(selectPostsMap);
 
   const post = posts[postId];
-  
+
   // Obtain the current like state from the user's browser
-  const likedPosts: {[key: string]: boolean} = JSON.parse(localStorage.getItem('likedPosts') || '{}');
-  const [isLiked, setIsLiked] = useState(postId in likedPosts && likedPosts[postId] ? true : false);
+  const likedPosts: { [key: string]: boolean } = JSON.parse(
+    localStorage.getItem("likedPosts") || "{}"
+  );
+  const [isLiked, setIsLiked] = useState(
+    postId in likedPosts && likedPosts[postId] ? true : false
+  );
+
+  const [isSharing, setIsSharing] = useState(false);
 
   const handleShare = () => {
     // TODO
@@ -62,10 +88,10 @@ export default function PostPage() {
         likes: post.likes + (isLiked ? -1 : 1),
       })
     );
-    
+
     // Update the like state of the user in the browser
     likedPosts[postId] = !isLiked;
-    localStorage.setItem('likedPosts', JSON.stringify(likedPosts));
+    localStorage.setItem("likedPosts", JSON.stringify(likedPosts));
 
     // Update the local state
     setIsLiked(!isLiked);
@@ -92,9 +118,14 @@ export default function PostPage() {
       <div className="blog-content">{parse(post?.content || "", options)}</div>
 
       {/** Share, comment, and like buttons */}
-      <div className="z-50 fixed flex flex-col right-3 sm:right-12 top-0 translate-y-[50vh] text-xl sm:text-2xl">
+      <div
+        className={
+          "z-50 fixed flex flex-col top-0 translate-y-[50vh] text-xl sm:text-2xl transform transition-all duration-1000 " +
+          (isSharing ? "-right-full" : "right-3 sm:right-12")
+        }
+      >
         {/** Share */}
-        <button>
+        <button onClick={() => setIsSharing(true)}>
           <FontAwesomeIcon icon={faShare} />
         </button>
 
@@ -105,10 +136,50 @@ export default function PostPage() {
 
         {/** Like */}
         <button onClick={handleLike}>
-          <FontAwesomeIcon icon={faHeart} color={(isLiked && 'red') || ''} beat={isLiked}/>
+          <FontAwesomeIcon
+            icon={faHeart}
+            color={(isLiked && "red") || ""}
+            beat={isLiked}
+          />
+        </button>
+      </div>
+
+      {/** Sharing overlay */}
+      <div
+        className={
+          "z-50 fixed flex flex-col top-0 translate-y-[50vh] text-xl sm:text-2xl gap-3 bg-gray-500 rounded-md p-3 bg-opacity-[25%] transform transition-all duration-1000 " +
+          (isSharing ? "right-3 sm:right-12" : "-right-full")
+        }
+      >
+        {/** Reddit */}
+        <RedditShareButton url={postUrl} title={shareMessage}>
+          <RedditIcon size={32} round />
+        </RedditShareButton>
+
+        {/** Twitter */}
+        <TwitterShareButton url={postUrl} title={shareMessage}>
+          <TwitterIcon size={32} round />
+        </TwitterShareButton>
+
+        {/** Whatsapp */}
+        <WhatsappShareButton url={postUrl} title={shareMessage}>
+          <WhatsappIcon size={32} round />
+        </WhatsappShareButton>
+
+        {/** Copy link */}
+        <button
+          onClick={() => {
+            navigator.clipboard.writeText(postUrl);
+          }}
+        >
+          <FontAwesomeIcon icon={faCopy} />
+        </button>
+
+        {/** Close */}
+        <button onClick={() => setIsSharing(false)}>
+          <FontAwesomeIcon icon={faClose} />
         </button>
       </div>
     </div>
   );
 }
-
