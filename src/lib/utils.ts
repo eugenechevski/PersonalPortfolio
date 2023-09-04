7b5191e040fbcda240ca17f96734c38a2c7bc45a
Eugene Chevski <eugenechevski@proton.me>
2023-09-04 13:01:53 -0400

    Adds the utility for generating a random hex string.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,26 @@
-import { imageUrlPattern } from "./constants"
+import { imageUrlPattern } from "./constants";
 
 export const isValidImgUrl = (url: string) => {
-    // First check the pattern
-    if (!RegExp(imageUrlPattern).test(url)) {
-        return false;
-    }
+  // First check the pattern
+  if (!RegExp(imageUrlPattern).test(url)) {
+    return false;
+  }
 
-    // check if the image can be loaded
-    const img = new Image();
-    img.src = url;
+  // check if the image can be loaded
+  const img = new Image();
+  img.src = url;
 
-    return img.complete;
-}
+  return img.complete;
+};
 
-export const strip = (str: string) => str.replace(/^\s+|\s+$/g, '');
+export const strip = (str: string) => str.replace(/^\s+|\s+$/g, "");
+
+export const randomHex = (length) => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let id = "";
+  for (let i = 0; i < length; i++) {
+    id += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return id;
+};
