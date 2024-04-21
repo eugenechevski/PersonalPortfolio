0fd458c1b56246d67ff17cac58c5a2246fab3b9c
Yauheni Khvashcheuski (Eugene Chevski) <48312706+eugenechevski@users.noreply.github.com>
2024-04-21 16:02:05 -0500

    adds the cover-letter page

---
+++ src/app/cover-letter/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Document, Page } from "react-pdf";
+import coverLetterDoc from "@/assets/docs/cover-letter.pdf";
+
+export default function CoverLetterPage() {
+  return (
+    <main className="primary-page flex justify-center items-center">
+      <Document file={coverLetterDoc} className="w-[90vw] sm:w-1/2 h-[90vh] overflow-auto">
+        <Page pageNumber={1} />
+      </Document>
+    </main>
+  );
+}
